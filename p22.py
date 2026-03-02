3ef48d347bdb3db73f45ea17e8ce9333d5fdad37
Payal <payallimje999999@gmail.com>
2026-03-02 22:20:24 +0530

    program2

---
+++ p22.py
@@ -0,0 +1,17 @@
+# Numpy array operation
+import numpy as np
+
+# Create array
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+# a) Number of dimensions
+print("Number of dimensions:", arr.ndim)
+
+# b) Shape of array
+print("Shape of array:", arr.shape)
+
+# c) Size of array
+print("Size of array:", arr.size)
+
+# d) Type of elements
+print("Data type of elements:", arr.dtype)
