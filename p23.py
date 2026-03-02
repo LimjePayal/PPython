713b7ea92a5a901e9d02fff8a04b3c04e4efdf6f
Payal <payallimje999999@gmail.com>
2026-03-02 22:27:17 +0530

    program2

---
+++ p23.py
@@ -0,0 +1,20 @@
+# Initialize vote count for 5 candidates
+# election voting system
+count = [0, 0, 0, 0, 0]
+spoilt = 0
+
+n = int(input("Enter number of voters: "))
+
+for i in range(n):
+    vote = int(input("Enter candidate number (1-5): "))
+
+    if 1 <= vote <= 5:
+        count[vote - 1] += 1
+    else:
+        spoilt += 1
+
+# Display results
+for i in range(5):
+    print("Votes for Candidate", i + 1, ":", count[i])
+
+print("Number of Spoilt Ballots:", spoilt)
