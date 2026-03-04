3826786a190ebf687a5ed0e972e5ed3deb8212ce
Payal <payallimje999999@gmail.com>
2026-03-04 22:20:26 +0530

    program2

---
+++ p25.py
@@ -0,0 +1,10 @@
+# numpy set differences
+import numpy as np
+
+array1 = np.array([0, 10, 20, 40, 60, 80])
+array2 = np.array([10, 30, 40, 50, 70, 90])
+
+result = np.setdiff1d(array1, array2)
+
+print("Set difference between two arrays:")
+print(result)
